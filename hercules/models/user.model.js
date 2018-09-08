73e1f2ca941d8f00f2b7cc7f83696b19b642a0d4
iSatyaMaharana <isatya.maharana@gmail.com>
2018-09-08 13:18:54 +0530

    Node service

---
+++ hercules/models/user.model.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+ var UserSchema = new Schema({
+    
+    first_name :{
+        type:String,
+        required : true,
+        
+    },
+    last_name :{
+        type:String,
+        required : true,
+        
+    },
+    email :{
+        type:String,
+        required : true,
+        trim: true,
+        unique:true,
+        match:/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+    },
+    
+    mobile :{
+        type:String,
+        required : true
+    },
+});
+
+
+
+const User = module.exports = mongoose.model('User', UserSchema);
